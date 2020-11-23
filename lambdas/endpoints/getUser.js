d694ced310ea68f9610fd39f4c2af14f85fd3e4b
James Thomas <codeactor@protonmail.com>
2020-11-23 10:22:29 +0000

    Add eslint and prettierrc

--- lambdas/endpoints/getUser.js
+++ lambdas/endpoints/getUser.js
@@ -1,20 +1,20 @@
-const Responses = require("../common/API_Responses");
-const Dynamo = require("../common/Dynamo");
+const Responses = require('../common/API_Responses');
+const Dynamo = require('../common/Dynamo');
 
 const tableName = process.env.tableName;
 
 exports.handler = async (event) => {
-  console.log("event", event);
+  console.log('event', event);
 
   if (!event.pathParameters || !event.pathParameters.ID) {
     // failed without an ID
-    return Responses._400({ message: "missing the ID from the path" });
+    return Responses._400({ message: 'missing the ID from the path' });
   }
 
   let ID = event.pathParameters.ID;
 
   const newUser = await Dynamo.get(ID, tableName).catch((err) => {
-    console.log("error in dynamo write", err);
+    console.log('error in dynamo write', err);
     return null;
   });
 
