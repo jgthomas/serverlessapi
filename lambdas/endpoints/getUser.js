1d731c4bf0f977107efad911535a8585ba626269
James Thomas <codeactor@protonmail.com>
2020-11-23 10:15:19 +0000

    Revert "Pass user ID in the payload body"
    This reverts commit f2cf96f6e6abcc930f3483cb3048ee520e00ed7c.

--- lambdas/endpoints/getUser.js
+++ lambdas/endpoints/getUser.js
@@ -4,28 +4,22 @@ const Dynamo = require("../common/Dynamo");
 const tableName = process.env.tableName;
 
 exports.handler = async (event) => {
-  //console.log("event", event);
+  console.log("event", event);
 
-  /*if (!event.pathParameters || !event.pathParameters.ID) {
+  if (!event.pathParameters || !event.pathParameters.ID) {
     // failed without an ID
     return Responses._400({ message: "missing the ID from the path" });
-  }*/
-
-  const data = JSON.parse(event.body);
-
-  if (!data || !data.ID) {
-    return Responses._400({ message: "No ID provided in payload" });
   }
 
-  //let ID = event.pathParameters.ID;
+  let ID = event.pathParameters.ID;
 
-  const newUser = await Dynamo.get(data.ID, tableName).catch(() => {
-    //console.log("error in dynamo write", err);
+  const newUser = await Dynamo.get(ID, tableName).catch((err) => {
+    console.log("error in dynamo write", err);
     return null;
   });
 
   if (!newUser) {
-    return Responses._400({ message: `No user found with ID: ${data.ID}` });
+    return Responses._400({ message: `No user with ID: ${ID}` });
   }
 
   return Responses._200({ newUser });
